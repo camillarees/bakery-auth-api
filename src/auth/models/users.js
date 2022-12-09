77429276f33eb74c58c20020785bd868522271d9
Camilla Rees <camilla.c.rees@gmail.com>
2022-12-08 18:12:29 -0700

    added middleware, user model, user routes, server and root level index

---
+++ src/auth/models/users.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const SECRET = process.env.SECRET;
+
+const userModel = (sequelize, DataTypes) => {
+  const model = sequelize.define('Users', {
+    username: {
+      type: DataTypes.STRING,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      required: true,
+    },
+    role: {
+      type: DataTypes.ENUM('user', 'waitStaff', 'headChef', 'manager'),
+      required: true,
+      defaultValue: 'user',
+    },
+    capabilities: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        const acl = {
+          user: ['read'],
+          waitStaff: ['read', 'update'],
+          headChef: ['read', 'create', 'update', 'delete'],
+          manager: ['read', 'create', 'update', 'delete'],
+        };
+        return acl[this.role];
+      },
+    },
+  });
+
+  model.beforeCreate(async (user) => {
+    let hashPass = await bcrypt.hash(user.password, 5);
+    user.password = hashPass;
+  });
+
+  model.authenticateBasic = async function (username, password) {
+    const user = await this.findOne({where: { username }});
+    const valid = await bcrypt.compare(password, user.password);
+    if(valid) { return user; }
+    throw new Error('Invalid User');
+  };
+
+  model.authenticateToken = async function (token) {
+    try{
+      const parsedToken = jwt.verify(token, SECRET);
+      const user = this.findOne({ where: { username: parsedToken.username }});
+      if (user) { return user; }
+      throw new Error('User Not Found');
+    } catch(e) {
+      throw new Error(e.message);
+    }
+  };
+
+  return model;
+};
+ 
+module.exports = userModel;
